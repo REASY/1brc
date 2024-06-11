13fe518c5b3ba54625fc64c33d2887cafe3fd005
Artavazd Balaian <reasyu@gmail.com>
2024-06-11 19:37:34 +0800

    fix: the path

--- scripts/run_benchmark_original.sh
+++ scripts/run_benchmark_original.sh
@@ -2,15 +2,20 @@
 
 set -eux
 
-git clone --depth=1 https://github.com/tumdum/1brc.git && cd 1brc &&  git checkout 34c761a9fa729fc121d01ef5abd25d16141f28e4
-git apply ../scripts/customize_original.patch
+cur_dir=$(pwd)
+patch_path="${cur_dir}/scripts/customize_original.patch"
+echo $patch_path
+
+cd /tmp
+git clone --depth=1 https://github.com/tumdum/1brc.git && cd 1brc && git checkout 34c761a9fa729fc121d01ef5abd25d16141f28e4
+git apply "${cur_dir}/scripts/customize_original.patch"
 
 rustflags="-C target-cpu=native" cargo build --release
 
 run_app="./target/release/rs /media/ramdisk/measurements.txt"
 
 function run_benchmark() {
-    hyperfine --warmup 1 --runs 4 --export-markdown "${1}_threads.md"
+    hyperfine --warmup 1 --runs 4 --export-markdown "${1}_threads.md" "${run_app} $1"
 }
 
 run_benchmark 1
