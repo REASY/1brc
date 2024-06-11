59d6ed0a72a934157f59f891999488e57caeab69
Artavazd Balaian <reasyu@gmail.com>
2024-06-11 17:12:19 +0800

    fix: ``` ./scripts/run_benchmark.sh: line 10: run_app_1: unbound variable ```

--- scripts/run_benchmark.sh
+++ scripts/run_benchmark.sh
@@ -7,10 +7,10 @@ cargo build --release
 run_app="./target/release/brc-app /media/ramdisk/measurements.txt"
 
 # Running dummy readers to understand what is the fastest throughput
-hyperfine --warmup 1 --runs 4 --export-markdown dummy.md "${run_app_1} naive_line_by_line_dummy" \
-  "${run_app_1} parse_large_chunks_as_bytes_dummy" \
-  "${run_app_1} parse_large_chunks_as_i64_dummy" \
-  "${run_app_1} parse_large_chunks_simd_dummy"
+hyperfine --warmup 1 --runs 4 --export-markdown dummy.md "${run_app} naive_line_by_line_dummy" \
+  "${run_app} parse_large_chunks_as_bytes_dummy" \
+  "${run_app} parse_large_chunks_as_i64_dummy" \
+  "${run_app} parse_large_chunks_simd_dummy"
   
 function run_benchmark() {
     hyperfine --warmup 1 --runs 4 --export-markdown "${1}_threads.md" \
