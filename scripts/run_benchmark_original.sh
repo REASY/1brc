84ac0ee8298c03c7199e9cb1fa7068b46b5876fc
Artavazd Balaian <reasyu@gmail.com>
2024-06-11 19:26:30 +0800

    feat: Add how to run benchmark for the original code

---
+++ scripts/run_benchmark_original.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+set -eux
+
+git clone --depth=1 https://github.com/tumdum/1brc.git && cd 1brc &&  git checkout 34c761a9fa729fc121d01ef5abd25d16141f28e4
+rustflags="-C target-cpu=native" cargo build --release
+
+run_app="./target/release/rs /media/ramdisk/measurements.txt"
+
+function run_benchmark() {
+    hyperfine --warmup 1 --runs 4 --export-markdown "${1}_threads.md"
+}
+
+run_benchmark 1
+run_benchmark 2
+run_benchmark 4
+run_benchmark 8
+run_benchmark 16
+run_benchmark 24
+run_benchmark 32
+run_benchmark 48
